a32690adbf43e9172c8f4023eb45f270b9b56306
Dennis Theisen <dennis@shopify.com>
2013-08-22 19:17:21 -0400

    Remove contains and ends_with filters for string columns to ensure indices are being used with LIKE queries

--- lib/active_admin/inputs/filter_string_input.rb
+++ lib/active_admin/inputs/filter_string_input.rb
@@ -18,13 +18,11 @@ module ActiveAdmin
       end
 
       def search_conditions
-        /_(contains|starts_with|ends_with)\z/
+        /_starts_with\z/
       end
 
       def default_filters
-        [ [I18n.t('active_admin.contains'),    'contains'],
-          [I18n.t('active_admin.starts_with'), 'starts_with'],
-          [I18n.t('active_admin.ends_with'),   'ends_with'] ]
+        [ [I18n.t('active_admin.starts_with'), 'starts_with'] ]
       end
 
     end
